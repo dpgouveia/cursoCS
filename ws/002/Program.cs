d79b7a57f41df121f5a30727f9837af55aaae545
Daniel Gouveia <daniel.gouveia@gmail.com>
2021-06-27 22:55:30 -0300

    Adicao de projeto referente a aula 002 do curso de C# da CF Cursos (Youtube)

---
+++ ws/002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CFB Cursos!");
+            if(args.GetLength(0) > 0) { 
+                Console.WriteLine("args[0] = " + args.GetValue(0));
+            } else { 
+                Console.WriteLine("Sem parametros");
+            }
+        }
+    }
+}
