4beeaf8eb28fbc445d8954dd17b10c7250f181be
Daniel Gouveia <daniel.gouveia@gmail.com>
2021-06-27 23:24:32 -0300

    Adicao de projeto referente a aula 004 do curso de C# da CF Cursos (Youtube)

---
+++ ws/004/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _004
+{
+    class Program
+    {
+        static int num = 64;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escopo de variaveis!");
+            int num = 256;
+            Console.WriteLine("Valor de num no metodo Main(): " + num);
+            Program.teste();
+            Console.WriteLine("Valor de num na class Program: " + Program.num);
+        }
+
+        static void teste() {
+            int num = 128;
+            Console.WriteLine("Valor de num em metodo teste(): " + num);
+        }
+
+    }
+}
