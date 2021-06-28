0c6ce979acac839f34eb0e87c9d59eb223898c7d
Daniel Gouveia <daniel.gouveia@gmail.com>
2021-06-27 22:34:09 -0300

    Projeto referente ao video 002 do curso (feito em VS)

---
+++ ws/002/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Aula02
+{
+	static void Main()
+	{
+		Console.WriteLine("Hello World Bitch!");
+		Console.WriteLine("Curso visto no Youtube (CFB Cursos)");
+		Console.ReadLine();
+	}
+}
