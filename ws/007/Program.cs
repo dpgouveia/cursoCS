316e85cff09bd5b38100477480b2e5fe93294723
Daniel Gouveia <daniel.gouveia@gmail.com>
2021-06-29 22:49:26 -0300

    Adicao de projeto referente a aula 007 do curso de C# da CF Cursos (Youtube)

---
+++ ws/007/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Constantes!");
+            const string canal = "CFB Cursos";
+            const double pi = 3.1415;
+
+            Console.WriteLine("Valor da variavel stringVar: {0}", canal);
+            Console.WriteLine("Valor da variavel pi: {0}", pi);
+
+            // canal = "Outro canal"; // nao rola mudar conteudo de constantes
+
+
+        }
+    }
+}
