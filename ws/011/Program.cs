82bd4453497e2e85b3f771072fd43ec3ca079ea6
Daniel Gouveia <daniel.gouveia@gmail.com>
2021-06-29 23:52:30 -0300

    Adicao de projeto referente a aula 011 do curso de C# da CF Cursos (Youtube)

---
+++ ws/011/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _011 { 
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Typecast!");
+
+            int i1 = 10;
+            float f1 = i1;
+            Console.WriteLine("Valor de i1: {0}", i1);
+            Console.WriteLine("Valor de f1: {0}", f1);
+
+            int i2 = (int) (f1 + 2.567); // typecast
+            Console.WriteLine("Valor de i2: {0}", i2);
+
+            long l1 = i2;
+            short s1 = (short) i2; // typecast
+            Console.WriteLine("Valor de l1: {0}", l1);
+            Console.WriteLine("Valor de s1: {0}", s1);
+
+        }
+    }
+}
