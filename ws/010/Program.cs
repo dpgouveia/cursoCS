0e4dde6ae4163b8be4c0e84f3ae0bad10081f12b
Daniel Gouveia <daniel.gouveia@gmail.com>
2021-06-29 23:41:31 -0300

    Adicao de projeto referente a aula 010 do curso de C# da CF Cursos (Youtube)

---
+++ ws/010/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _010 {
+    class Program {
+
+        enum DiasSemana { Dom, Seg, Ter, Qua, Qui, Sex, Sab };
+
+        static void Main(string[] args) {
+            Console.WriteLine("Enumeradores!");
+
+            DiasSemana ds = DiasSemana.Dom;
+            DiasSemana ds2 = (DiasSemana) 3; // Qua
+            Console.WriteLine("Valor de ds       : {0}", ds);
+            Console.WriteLine("Valor de ds2      : {0}", ds2);
+            Console.WriteLine("Valor de (int) ds2: {0}", (int) ds2);
+
+        }
+        
+    }
+}
